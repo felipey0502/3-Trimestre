d010de3fbfa34590c168d7cfc80ec7d6dcd5bebb
felipey0502 <felipe.inoue@escola.pr.gov.br>
2025-08-29 09:41:45 -0300

    Add files via upload

---
+++ 3-Trimestre-main/3-Trimestre-main/atividade 4/main.js
@@ -0,0 +1,14 @@
+// cria referência ao form e aos elementos de resposta (pelo seu id)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um "ouvinte" de evento, acionado quando botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const quilo = Number(frm.inQuilo.value);
+    const consumo = Number(frm.inConsumo.value);
+
+    const valor = (quilo/ 1000) * consumo;
+    resp.innerText = `Valor a pagar: R$ ${valor.toFixed(2)};`
+
+    e.preventDefault();
+})
